45a1b28f2db7fb1c33f6a2250349276fb92ba961
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-03-08 19:13:15 +0000

    singlePlayer done

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ import math
 #Tamanho da janela
 WIDTH = 1280
 HEIGHT = 720
+TICKRATE = 15
 
 
 '''
@@ -53,6 +54,9 @@ print(rotate_triangle(tr,90))
             print(key)
 '''
 
+#Lista para guardar os tiros
+shots = []
+
 #Cores
 black = (0,0,0)
 grey = (128,128,128)
@@ -91,18 +95,67 @@ def generate_triangle(center,width,height,angle):
 #Player tunes
 pWidth = 32
 pHeight = 48
-pAcc = 0.35
-pShieldCooldown = 180
-pShotCooldown = 120
+pAcc = 0.35*(60/TICKRATE)
+pShieldCooldown = int(120*(TICKRATE/60))
+pShieldActive = int(80*(TICKRATE/60))
+pShotCooldown = int(180*(TICKRATE/60))
+pDefaultHealth = int(100)
+pDefaultRotation = int(2*(60/TICKRATE))
+sDefaultVel = int(10*(60/TICKRATE))
+sDefaultTTL = int(120*(TICKRATE/60))
+sDefaultSize = int(30)
 
 
 class Shot():
     def __init__(self,x,y,ang,color):
         self.x = x
         self.y = y 
+        self.ang = ang * (math.pi/180)
         self.color = color
-        self.ttl = pShotCooldown
-        self.ang = ang
+
+        self.vel = sDefaultVel
+        self.ttl = sDefaultTTL
+        self.size = sDefaultSize
+        
+        self.a = (self.x,self.y)
+        self.b = (self.x + self.size*math.cos(self.ang),self.y + self.size*math.sin(self.ang))
+
+        shots.append(self)
+
+    def move(self):
+        self.x += self.vel*math.cos(self.ang)
+        self.y += self.vel*math.sin(self.ang)
+        
+        if self.x > WIDTH:
+            self.x -= WIDTH
+        
+        if self.y > HEIGHT:
+            shots.remove(self)
+
+        if self.x < 0:
+            self.x = WIDTH + self.x
+
+        if self.y < 0:
+            shots.remove(self)
+
+    
+    def draw(self,win):
+        self.a = (self.x,self.y)
+        self.b = (self.x + self.size*math.cos(self.ang),self.y + self.size*math.sin(self.ang))
+        
+        pg.draw.line(win, self.color, self.a, self.b, 2)
+
+    def update(self):
+        self.move()
+        if self.ttl > 0:
+            self.ttl -= 1
+        else:
+            shots.remove(self)
+
+        self.a = (self.x,self.y)
+        self.b = (self.x + self.size*math.cos(self.ang),self.y + self.size*math.sin(self.ang))
+
+
 
 
 
@@ -118,6 +171,8 @@ class Player(pg.sprite.Sprite):
         self.shieldCooldown = pShieldCooldown
 
         self.shotCooldown = pShotCooldown
+
+        self.health = pDefaultHealth
 
         self.acc = pAcc
         self.velX = 0
@@ -136,11 +191,12 @@ class Player(pg.sprite.Sprite):
         else:
             fill = 2
             if self.shieldCooldown == 0:
-                pg.draw.circle(win,self.color,a,4)
-                pg.draw.circle(win,self.color,b,4)
+                pg.draw.circle(win,self.color,(self.x,self.y),8)
+                #pg.draw.circle(win,self.color,a,4)
+                #pg.draw.circle(win,self.color,b,4)
 
         if self.shotCooldown == 0:
-            pg.draw.circle(win,red,c,4)
+            pg.draw.circle(win,grey,c,4)
 
         pg.draw.polygon(win, self.color, self.triangle, fill)
 
@@ -171,14 +227,12 @@ class Player(pg.sprite.Sprite):
         if self.y < 0:
             self.y = 0#HEIGHT + self.y
 
-
-        self.triangle = generate_triangle((self.x,self.y), self.w, self.h, self.ang)
+    
+        
         
 
 
     def key_press(self):
-        rot = 5 #Angulo de rotacao em º 
-
         keys = pg.key.get_pressed()
         
         if keys[pg.K_w]:
@@ -186,30 +240,31 @@ class Player(pg.sprite.Sprite):
             self.velY += self.acc*(math.sin(self.ang * (math.pi/180)))
 
         if keys[pg.K_a]:
-            if self.ang - rot < -180:
-                self.ang = 180 - (rot - (self.ang + 180))
+            if self.ang - pDefaultRotation < -180:
+                self.ang = 180 - (pDefaultRotation - (self.ang + 180))
             else:
-                self.ang -= rot
+                self.ang -= pDefaultRotation
 
         if keys[pg.K_s]:
             self.velX -= self.acc*(math.cos(self.ang * (math.pi/180)))
             self.velY -= self.acc*(math.sin(self.ang * (math.pi/180)))
 
         if keys[pg.K_d]:             
-            if self.ang + rot > 180:
-                self.ang = -180 + (rot - (180 - self.ang))
+            if self.ang + pDefaultRotation > 180:
+                self.ang = -180 + (pDefaultRotation - (180 - self.ang))
             else:
-                self.ang += rot
+                self.ang += pDefaultRotation
 
         if keys[pg.K_e]:
             if self.shieldCooldown == 0 and not self.shield:
                 self.shield = True
-                self.shieldCooldown = 60
+                self.shieldCooldown = pShieldActive
 
         if keys[pg.K_SPACE]:
             if self.shotCooldown == 0:
-                self.shotCooldown = 120
-
+                self.shotCooldown = pShotCooldown
+                x,y = self.triangle[2]
+                Shot(x,y,self.ang,self.color)
 
     def update(self):
         self.key_press()
@@ -220,24 +275,29 @@ class Player(pg.sprite.Sprite):
             self.shieldCooldown -= 1
         if self.shieldCooldown == 0 and self.shield:
             self.shield = False
-            self.shieldCooldown = 180
+            self.shieldCooldown = pShieldCooldown
 
         if self.shotCooldown > 0:
             self.shotCooldown -= 1
+        
+        self.triangle = generate_triangle((self.x,self.y), self.w, self.h, self.ang)
 
 
 
 
 
-
+#Definições do display 
 win = pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption("KSD")
 background = black
-all_sprite = pg.sprite.Group
+#all_sprite = pg.sprite.Group
+
 
 
 def redraw_win(win,player):
     win.fill(background)
+    for s in shots:
+        s.draw(win)
     player.draw(win)
     pg.display.flip()
 
@@ -249,7 +309,7 @@ def main():
 
 
     while run:
-        clock.tick(60)
+        clock.tick(TICKRATE)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
@@ -257,6 +317,8 @@ def main():
 
         if not run: break
 
+        for s in shots:
+            s.update()
         p.update()
         redraw_win(win,p)
 
