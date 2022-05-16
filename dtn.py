9822cde5d45dc0cac4bab2f939f85aadde1cbeeb
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-05-16 18:20:06 +0100

    iadadadadad

--- dtn.py
+++ dtn.py
@@ -183,7 +183,7 @@ class Forwarder():
         else:
             nextHop = self.neighbours.best_neighbour_addr()
             if nextHop:
-                self.outSocket.sendto(data,(nextHop,self.dtn_port))
+                self.outSocket.sendto(data,(nextHop,util.mobilePort))
             else:
                 print("Packet dropped")
 
