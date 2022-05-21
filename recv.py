cad283dfbea1ed6d894edc5030f3f0050bdbbacc
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-05-21 20:37:26 +0100

    fix

--- recv.py
+++ recv.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 inSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-inSocket.bind(sys.argv[1],int(sys.argv[2]))
+inSocket.bind((sys.argv[1],int(sys.argv[2])))
 
 while True:
     data,addr = inSocket.recvfrom(1024)
