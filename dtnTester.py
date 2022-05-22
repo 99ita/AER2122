950875c1d922b51fe15ae1e3a0a0628d75d96ce8
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-05-22 18:43:10 +0100

    messagefix

--- dtnTester.py
+++ dtnTester.py
@@ -9,6 +9,7 @@ fwd = dtn.Forwarder(sys.argv[1])
 message = "message"
 i = 0   
 while True:
+    message = "message"
     message += f'{i}'
     print(f"sending: '{message}'")
     fwd.send_packet(bytes(message,'utf-8'))
