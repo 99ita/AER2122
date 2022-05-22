7d85751db6e25486a3dbaf9f8562d86a67ea557d
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-05-23 00:22:07 +0100

    prints

--- dtnTester.py
+++ dtnTester.py
@@ -11,7 +11,7 @@ i = 0
 while True:
     message = "message"
     message += f'{i}'
-    print(f"sending: '{message}'")
+    #print(f"sending: '{message}'")
     fwd.send_packet(bytes(message,'utf-8'),fst = True, server_pair = (sys.argv[2],int(sys.argv[3])))
     time.sleep(1)
     i+=1
