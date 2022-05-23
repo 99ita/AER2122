44d7611f02774d80bec29aede552b217ae0ac4fa
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-05-23 18:18:31 +0100

    fr

--- util.py
+++ util.py
@@ -11,7 +11,7 @@ mobilePort = 5556
 #Game constants
 wWidth = 860 
 wHeight = 640 
-framerate = 4 #FPS
+framerate = 20 #FPS
 netrate = 1 #Every <netrate> frames a packet will be sent to server
 backgroundFile = 'dark-space-minimal-art-4k-ll.jpg' 
 
