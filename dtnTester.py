9aa1d37be76b4b92155fad51ee29923d78590e64
Eduardo Araujo <eduardojafaraujo@gmail.com>
2022-06-11 19:41:46 +0100

    test

--- dtnTester.py
+++ dtnTester.py
@@ -4,11 +4,9 @@ import time
 
 fwd = dtn.Forwarder(sys.argv[1])
 
-message = "message"
 i = 0   
 while True:
-    message = "message"
-    message += f'{i}'
+    message = f'{i}'
     #print(f"sending: '{message}'")
     fwd.send_packet(bytes(message,'utf-8'),fst = True, server_pair = (sys.argv[2],int(sys.argv[3])))
     time.sleep(1)
--- recv.py
+++ recv.py
@@ -3,8 +3,16 @@ import sys
 
 inSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 inSocket.bind((sys.argv[1],int(sys.argv[2])))
-
+fst = True
+last = 0
 while True:
     data,addr = inSocket.recvfrom(1024)
+    data = int(data.decode('utf-8'))
+    if fst:
+        fst = False
+        last = data
+    else:
+        if data-last > 1:
+            print("Packet Lost!")
+        last = data
     print(data)
-    print(addr)
